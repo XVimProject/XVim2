0b8803ea0100ab1f5f19785393b82342afce41c2
Nicholas Won <devnicholas47@gmail.com>
2019-12-29 17:00:21 +0900

    Update nm_swift_demangle.py

--- Tools/nm_swift_demangle.py
+++ Tools/nm_swift_demangle.py
@@ -14,15 +14,15 @@ modules = ['/Applications/Xcode.app/Contents/SharedFrameworks/SourceEditor.frame
           ,'/Applications/Xcode.app/Contents/SharedFrameworks/SourceKit.framework/SourceKit']
 with open('list.txt', "w") as f3:
     for module in modules: 
-        cmd = 'nm' + ' ' + module
+        cmd = 'nm {}'.format(module)
         with open('cmd.txt', "w") as handle:
             subprocess.run(cmd, shell=True, stdout=handle)
         with open('cmd.txt') as handle:
             for line in handle:
                 words = line.split()
                 for word in words:
-                    if len(word) >= 2 and word[0] == '_' and word[1] == '$':
-                        cmd2 = "swift demangle '" + word + "'" 
+                    if word.startswith('_$'):
+                        cmd2 = "swift demangle '{}'".format(word)
                         with open('cmd2.txt', "w") as handle2:
                             subprocess.run(cmd2, shell=True, stdout=handle2)
                         with open('cmd2.txt') as handle2:
