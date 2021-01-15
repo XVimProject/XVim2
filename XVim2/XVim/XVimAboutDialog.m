ad9ed80a27eb58c178d6e89422ae8f7b24cfc30a
r-plus <anasthasia.r@gmail.com>
2021-01-15 22:50:19 +0900

    Add running in rosetta value to xvim info

--- XVim2/XVim/XVimAboutDialog.m
+++ XVim2/XVim/XVimAboutDialog.m
@@ -9,6 +9,8 @@
 #import "XVimAboutDialog.h"
 #import "../../gitrevision.h"
 #import "XVim.h"
+#import <sys/types.h>
+#import <sys/sysctl.h>
 
 @interface XVimAboutDialog ()
 
@@ -21,6 +23,7 @@
     NSString* format = @"XVim2 revision : %@\n"
                        @"OS Version : %@\n"
                        @"Xcode Version : %@\n"
+                       @"Rosetta: %@\n"
                        @"\n"
                        @"--- .xvimrc ---\n"
                        @"%@\n"
@@ -34,9 +37,24 @@
                       GIT_REVISION,
                       NSProcessInfo.processInfo.operatingSystemVersionString,
                       [NSBundle.mainBundle.infoDictionary objectForKey:@"CFBundleShortVersionString"],
+                      self.isRunningInRosetta ? @"YES" : @"NO",
                       rc];
 
     return info;
+}
+
+// https://developer.apple.com/videos/play/wwdc2020/10686/?time=870
+- (BOOL)isRunningInRosetta
+{
+    int ret = 0;
+    size_t size = sizeof(ret);
+    // Call the sysctl and if successful return the result
+    if (sysctlbyname("sysctl.proc_translated", &ret, &size, NULL, 0) != -1)
+        return ret == 1;
+    // If "sysctl.proc_translated" is not present then must be native
+    if (errno == ENOENT)
+        return NO;
+    return NO;
 }
 
 - (void)windowDidLoad
