60206efee67e0345e19902b756c159ea6c6d2dc3
r-plus <anasthasia.r@gmail.com>
2021-04-21 12:42:04 +0900

    Revert "ci: try to fail unit test"
    This reverts commit a7e010851a3a9217ffb516d5d5e6ea22902e1772.

--- XVim2/UnitTest/XVimTester+Operator.m
+++ XVim2/UnitTest/XVimTester+Operator.m
@@ -406,7 +406,7 @@
                                       @"        ccc\n" // 16
                                       @"    ddd\n" // 28
                                       @"    eee\n" // 38
-                                      @"        ffftest"; // 46
+                                      @"        fff"; // 46
 
     static NSString* lshift_result5_2 = @"\taaa\n"
                                         @"\tbbb\n";
